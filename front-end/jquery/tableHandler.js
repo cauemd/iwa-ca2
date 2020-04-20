c6813a9d9b8a9354a4cf4226bd0ab62cd2627258
Caue Meireles Duarte <cauemd91@gmail.com>
2020-04-20 16:26:04 +0000

    started working on the front-end index.html. Added required  bootstrap files to the project. Removed script from inside the html and moved it to the tableHandler.js file

--- front-end/jquery/tableHandler.js
+++ front-end/jquery/tableHandler.js
@@ -1,19 +1,23 @@
-function draw_table()
-{
-	$("#results").empty();
-	$.getJSONuncached = function (url)
-	{
-		return $.ajax(
-		{
-			url: url,
-			type: 'GET',
-			cache: false,
-			success: function (html)
-			{
-				$("#results").append(html);
-				select_row();
-			}
-		});
-	};
-	$.getJSONuncached("/get/html")
+
+//Fetches the data from the mongo db and returns it as in json format
+function fetchingData(){
+    fetch("/monsters")
+        .then(response => response.json())
+        .then(json => draw_table(json));
+    // const json = await response.json();
+    // // for(let i = 0; i < json.length; i++){
+    // //     console.log(json[i]);
+    // // }   
+    // return json;
+}
+
+
+function draw_table(data){
+    for(let i = 0; i < data.length; i++){
+        console.log(data[i].name);
+    }
+    
+    
 };
+
+fetchingData();
